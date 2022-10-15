11928cbc4000f6537962d2dc3bd3687a573da9b3
pablofilipe <pablo.filip3@gmail.com>
2022-10-14 23:07:30 -0300

    Atualizando regex.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 function tratarKey() {
     let chaveNfe = document.getElementById('keyNfe').value
-    document.getElementById('keyNfe').value = chaveNfe.replace(/\s/g, '')
+    // document.getElementById('keyNfe').value = chaveNfe.replace(/\s/g, '')
+    document.getElementById('keyNfe').value = chaveNfe.replace(/[^a-z0-9]/gi, '')
 }
 
 function clearInput() {
